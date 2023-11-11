1ac4b752dda4fa21666539797e8fd2639da3265c
Bepis <artemka2003may@gmail.com>
2023-11-11 13:39:28 +0300

    inicilize

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import "./Header.scss"
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div>@</div>
+      <nav>
+        <a href="#">ссылка</a>
+        <a href="#">ссылка</a>
+        <a href="#">ссылка</a>
+        <a href="#">ссылка</a>
+      </nav>
+      <div className="contacts">88005553535</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header";
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <div className="main">main</div>
+      <footer>footer</footer>
+    </div>
+  );
+};
+
+export default Home;
